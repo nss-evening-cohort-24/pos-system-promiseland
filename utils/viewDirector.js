a8dbbfe75d9ebceb56cb3e258fd514d66bfdc05e
paulpatterson <paul@pattersonmultimedia.com>
2023-09-21 19:15:31 -0500

    changes with origin domEvents

--- utils/viewDirector.js
+++ utils/viewDirector.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import logoutButton from '../components/Buttons/logoutButton';
 import loginButton from '../components/Buttons/loginButton';
 import client from './client';
 import startApp from './startApp';
-
 
 const ViewDirectorBasedOnUserAuthStatus = () => {
   firebase.initializeApp(client);
