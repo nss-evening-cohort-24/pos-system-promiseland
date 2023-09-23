d148229c48b8be816a06cba1fafeda01de3c61ae
paulpatterson <paul@pattersonmultimedia.com>
2023-09-23 11:44:35 -0500

    able to CRUD order

--- pages/homeScreen.js
+++ pages/homeScreen.js
@@ -6,7 +6,7 @@ const homeScreen = (user) => {
   domString += `<h1> Welcome ${user.displayName} !</h1>
   <div class="button-row">
     <button type="button" id='view-orders' class"btn btn-success">View Orders</button>
-    <button type="button"  id='create-orders-btn' class="btn btn-info">Create An Order</button>
+    <button type="button"  id='create-orders' class="btn btn-info">Create An Order</button>
     <button type="button" id='view-revenue' class= btn-warning">View Revenue</button>
   </div>`;
   renderToDom('#buttons', domString);
