6b39c7b660a2d496a012ac63e29a9bca3be01145
paulpatterson <paul@pattersonmultimedia.com>
2023-09-23 12:39:51 -0500

    added logo

--- components/Buttons/loginButton.js
+++ components/Buttons/loginButton.js
@@ -2,7 +2,7 @@ import { signIn } from '../../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<div class="login-screen"><div> class="login-header"><img src=logo.png"><div><div class=login-button><button id="google-auth" class="btn btn-danger">Login to Get Started</button></div></div></div></div>';
+  const domString = '<div class="login-screen"><div class="login-header"><img src="https://user-images.githubusercontent.com/29741570/205346767-a182560c-64a6-4cfa-80b3-0d64cf998242.png"><div><div class=login-button><button id="google-auth" class="btn btn-danger">Login to Get Started</button></div></div></div></div></div>';
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
